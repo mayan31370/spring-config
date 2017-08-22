cdb68da4751244bcf13884b26c642aceccae19f2
马岩 <yan.ma@counect.com>
2017-08-22 17:34:19 +0800

    remove eureka-client

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM registry.cn-hangzhou.aliyuncs.com/counect_data/openjdk-8-jre-alpine-fixed-t
 ENV CONFIG_SERVER_GIT_URL repo-url
 ENV CONFIG_SERVER_GIT_PASSWORD password
 ENV CONFIG_SERVER_GIT_USERNAME username
-ENV EUREKA_SERVER_URL eureka-url
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","-Dserver.port=8080","/app.jar"]
 ADD target/*.jar app.jar
--- src/main/java/com/counect/microservice/ConfigApplication.java
+++ src/main/java/com/counect/microservice/ConfigApplication.java
@@ -2,13 +2,10 @@ package com.counect.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
 public class ConfigApplication {
 
   public static void main(String[] args) {
